b0ce9fa70354bcc0d9901ac840ef3b6314828c3f
Guijun1 <511067632@qq.com>
2019-06-16 18:05:15 +0800

    更新主线

--- src/com/atguigu/egit/test/EgitTest.java
+++ src/com/atguigu/egit/test/EgitTest.java
@@ -4,10 +4,6 @@ public class EgitTest {
 
 	public static void main(String[] args) {
 		System.out.println("are you ok ?");
-		System.out.println("改为分支are you ok ?");
-		System.out.println("改为分支are you ok ?");
-		
-		
 	}
 
 }
