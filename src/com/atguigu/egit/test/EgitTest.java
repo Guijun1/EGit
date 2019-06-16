68501aaf93640ba439e2ca8ff038cb65fcb6ea64
Guijun1 <511067632@qq.com>
2019-06-16 17:59:38 +0800

    分支

--- src/com/atguigu/egit/test/EgitTest.java
+++ src/com/atguigu/egit/test/EgitTest.java
@@ -4,6 +4,10 @@ public class EgitTest {
 
 	public static void main(String[] args) {
 		System.out.println("are you ok ?");
+		System.out.println("改为分支are you ok ?");
+		System.out.println("改为分支are you ok ?");
+		
+		
 	}
 
 }
